7bd607be144f7236a0201a00406f35ad97e27439
Villy Siu <villysiu@gmail.com>
2023-12-28 13:40:00 -0800

    add extra fields in MenuItem

--- LittleD/WineAPI/models.py
+++ LittleD/WineAPI/models.py
@@ -10,11 +10,15 @@ class Category(models.Model):
     def __str__(self)-> str:
         return self.title
 
+    
 class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
-    featured = models.BooleanField(db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True)
-    
+    origin = models.CharField(max_length=255)
+    point = models.IntegerField()
+    varietal = models.CharField(max_length=255)
+    description = models.TextField()
+    year = models.SmallIntegerField()
     def __str__(self):
         return self.title
--- LittleD/WineAPI/urls.py
+++ LittleD/WineAPI/urls.py
@@ -3,4 +3,6 @@ from . import views
   
 urlpatterns = [ 
     path('categories', views.Categories.as_view()),
+    path('categories/<int:pk>', views.SingleCategory.as_view()),
+
 ]
